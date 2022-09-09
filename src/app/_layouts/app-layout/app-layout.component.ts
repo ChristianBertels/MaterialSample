15a7070c291d657f8c933bb93c90a63e497ff586
Christian Bertels <christian.bertels@blickle.com>
2022-09-09 10:56:46 +0200

    pwa

--- src/app/_layouts/app-layout/app-layout.component.ts
+++ src/app/_layouts/app-layout/app-layout.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Project, ProjectService } from './../../project.service';
 
 @Component({
   selector: 'app-app-layout',
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.scss']
 })
-export class AppLayoutComponent {
-
+export class AppLayoutComponent implements OnInit{
+  projects: Project[] = [];
+    
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private projectService: ProjectService) {}
 
+  ngOnInit() {
+    this.projects = this.projectService.loadProjekts();
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MaterialSample';
+
+  constructor(private swUpdate: SwUpdate){}
+
+  ngOnInit() {
+    if(this.swUpdate.isEnabled){
+      this.swUpdate.versionUpdates.subscribe(async (version)=>{
+        if(version.type == 'VERSION_DETECTED'){
+          await this.swUpdate.activateUpdate();
+          window.location.reload();
+        }        
+      });
+    }
+  }
 }
